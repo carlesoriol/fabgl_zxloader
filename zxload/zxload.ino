07068a9e4754280e13c013d0d5a56b912660686d
Carles Oriol <carles@enging.com>
2020-08-23 15:42:44 +0200

    ZXBitmap structure

--- zxload/zxload.ino
+++ zxload/zxload.ino
@@ -1,4 +1,3 @@
-
 #include "fabgl.h"
 
 fabgl::VGAController VGAController;
@@ -16,10 +15,31 @@ const uint8_t zxpalette[2][8] = //with and without bright 2 bit pairs
   { 0b11000000, 0b11110000, 0b11000011, 0b11100011, 0b11001100, 0b11111100, 0b11001111, 0b11111111 }  // 1 bright
 };
 
+struct ZXBitmap : public Bitmap
+{
+  void fromMemory( const uint8_t *pmemscreen )
+  {
+    for (int y=0; y< 192; y++)    
+      for (int x=0; x< 32; x++)
+      {     
+        uint8_t bytevalue = pmemscreen[((y/64)*64 + (y%8)*8 + (y/8)%8) * 32 + x];
+        uint8_t byteattrib = pmemscreen[(y/8)*32 + x + 6144];
+        for(int bitp = 0; bitp < 8; bitp++)
+            data[y*256+x*8+bitp] = zxpalette [((byteattrib & 0b1000000)>>6)&1]
+                                             [ (byteattrib >> ((bytevalue & (0b10000000 >> bitp)) ? 0 : 3)) & 0b111];
+       }
+  }
+  
+  ZXBitmap ( ) : Bitmap(256, 192, NULL, PixelFormat::RGBA2222, RGB888(0, 0, 0)){
+    //allocate(); why private? There's no empty contructor!!
+    data = (uint8_t*) malloc(256*192);
+    dataAllocated = true;
+  }
+  ZXBitmap ( const uint8_t *pmemscreen ) : ZXBitmap() { fromMemory(pmemscreen); }  
+};
+
 void setup() 
 {     
-  Serial.begin(115200);
-
   VGAController.begin();
   VGAController.setResolution(VGA_320x200_75Hz, 320, 200); 
 
@@ -27,23 +47,7 @@ void setup()
   canvas.clear();
   canvas.waitCompletion();
 
-  Bitmap zxscreen = Bitmap(256, 192, NULL, PixelFormat::RGBA2222, RGB888(0, 0, 0));
-  zxscreen.width = 256;
-  zxscreen.height = 192;
-  //zxscreen.allocate(); why protected? There's no empty contructor!!
-  zxscreen.data = (uint8_t*) malloc(256*192);
-  zxscreen.dataAllocated = true;
-
-  for (int y=0; y< 192; y++)    
-    for (int x=0; x< 32; x++)
-    {     
-      uint8_t bytevalue = KnightLore_data[((y/64)*64 + (y%8)*8 + (y/8)%8) * 32 + x];
-      uint8_t byteattrib = KnightLore_data[(y/8)*32 + x + 6144];
-      for(int bitp = 0; bitp < 8; bitp++)
-          zxscreen.data[y*256+x*8+bitp] = zxpalette [((byteattrib & 0b1000000)>>6)&1]
-                                                    [ (byteattrib >> ((bytevalue & (0b10000000 >> bitp)) ? 0 : 3)) & 0b111];
-    }
-
+  ZXBitmap zxscreen( KnightLore_data );
   canvas.drawBitmap( 32, 4, &zxscreen );
   canvas.waitCompletion();
 }
